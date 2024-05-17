249a049e5e269f98c2a301004d864952102b76e5
suryanshvermaa <suryanshv.ug23.ee@nitp.ac.in>
2024-05-17 21:58:44 +0530

    added edit functionality

---
+++ Task1/Dev/README.md
@@ -0,0 +1,53 @@
+
+
+# Todo List Project
+
+## Overview
+
+This is a simple todo list project built using HTML, CSS, and JavaScript. The project aims to provide users with a straightforward interface for managing their tasks.
+
+## Features
+
+- Add new tasks
+- edit task
+- Delete tasks
+
+
+## Setup
+
+To run this project locally, follow these steps:
+
+1. Clone the repository:
+
+```bash
+git clone <repository-url>
+```
+
+2. Navigate to the project directory:
+
+```bash
+cd Team405_Tasks
+```
+
+3. Open `index.html` in your preferred web browser.
+
+## Demo
+sample video is provided here -: 
+   
+
+
+https://github.com/dev04sa/Team405_Tasks/assets/129666293/22c8a24d-071b-43f2-979b-6d10b77a3d1a
+
+
+
+
+## Contributing
+
+Contributions are welcome! If you'd like to contribute to this project, please follow these steps:
+
+1. Fork the repository.
+2. Create a new branch for your work: `git checkout -b branch-name`.
+3. create a folder with your-name with <your-name> and create files same as provided in sample folder with name Dev.
+3. Commit your changes: `git commit -m 'Add some feature'`.
+4. Push to the branch: `git push origin branch-name`.
+5. Submit a pull request.
--- Task1/Suryansh/script.js
+++ Task1/Suryansh/script.js
@@ -11,6 +11,49 @@ function deleteTodo(i){
     displayTodos()
 }
 
+
+function updateTodo(i){
+    let inputText=document.querySelector('.updateInput');
+    let text=inputText.value;
+  
+  
+  
+    let inputDate=document.querySelector('.updateDate');
+    
+    todos.splice(i,1,{
+        
+        todoText: text,
+        todoDate: inputDate.value
+    })
+    console.log({
+        
+        todoText: text,
+        todoDate: inputDate.value
+    })
+
+    let update=document.querySelector('.updateTodo');
+    update.innerHTML='';
+    displayTodos()
+   
+}
+function editTodo(i){
+    let update=document.querySelector('.updateTodo');
+    update.innerHTML=`
+
+    <div class="  items-center z-100 flex flex-col items-center ">
+
+    <h1 class="text-center text-3xl font-bold  z-100 mt-20"> Edit Task</h1>
+    <div class="flex flex-row justify-center mt-6  z-100">
+     
+       <input type="text" class="px-4 py-2 border-solid border-2 rounded-md border-gray-700 updateInput  value="${todos[i].todoText}" placeholder="Enter Todo here">
+       <input type="date" name="date" class="px-3 py-2 border-solid border-2 rounded-md border-gray-700 mx-2 updateDate" value="${todos[i].todoDate}" >
+       <button class="p-2 bg-green-400 rounded-md  px-3 font-semibold " onclick="updateTodo(${i})">Update</button>
+  
+    </div>
+    </div>
+    `
+}
+
 function displayTodos(){
     let allTodos=document.querySelector('.addedTodos');
     let innerHtml='';
@@ -19,9 +62,10 @@ function displayTodos(){
             
             innerHtml+=`
             <div class="flex flex-row justify-center mt-4 items-center ">
-            <p class="px-4 w-[220px]" >${todos[i].todoText}</p>
-            <p  class="px-3 w-[150px] mx-2" > ${todos[i].todoDate}</p>
+            <input class="px-4 py-2 w-[220px] " value="${todos[i].todoText}" readonly>
+            <input class="px-3 py-2 w-[150px] mx-2"value=" ${todos[i].todoDate}">
             <button class=" bg-pink-400 rounded-md py-2 px-3  font-semibold" onclick="deleteTodo(${i})">Delete</button> 
+            <button class=" bg-pink-400 rounded-md py-2 px-3 ml-2  font-semibold" onclick="editTodo(${i})">Edit</button> 
             </div>
            
             `
