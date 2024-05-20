6fc0ce6439cfbcd5018a62c617fba9ae1f3ba278
suryanshvermaa <suryanshv.ug23.ee@nitp.ac.in>
2024-05-20 17:24:54 +0530

    added responsiveness for all devices

--- Task1/Suryansh/script.js
+++ Task1/Suryansh/script.js
@@ -39,15 +39,16 @@ function updateTodo(i){
 function editTodo(i){
     let update=document.querySelector('.updateTodo');
     update.innerHTML=`
-
-    <div class="  items-center z-100 flex flex-col items-center ">
-
-    <h1 class="text-center text-3xl font-bold  z-100 mt-20"> Edit Task</h1>
-    <div class="flex flex-row justify-center mt-6  z-100">
      
-       <input type="text" class="px-4 py-2 border-solid border-2 rounded-md border-gray-700 updateInput  value="${todos[i].todoText}" placeholder="Enter Todo here">
-       <input type="date" name="date" class="px-3 py-2 border-solid border-2 rounded-md border-gray-700 mx-2 updateDate" value="${todos[i].todoDate}" >
-       <button class="p-2 bg-green-400 rounded-md  px-3 font-semibold " onclick="updateTodo(${i})">Update</button>
+    <div class="   z-100  mt-6 ">
+    
+   
+    <h1 class="text-center text-3xl font-bold  z-100 "> Edit Task</h1>
+    <div class="flex sm:flex-row sm:justify-center flex-col sm:mt-6">
+     
+       <input type="text" class="px-4 py-2 border-solid border-2 rounded-md border-gray-700  sm:mx-0 mx-2  updateInput"  value="${todos[i].todoText}" placeholder="Enter Todo here">
+       <input type="date" name="date" class="px-3 py-2 border-solid border-2 rounded-md border-gray-700 sm:mx-2 sm:m-0 m-2 updateDate" value="${todos[i].todoDate}" >
+       <button class="p-2 bg-green-400 rounded-md  px-3 font-semibold sm:mx:0 sm:mb-0 sm:mx-0 mb-2 mx-2" onclick="updateTodo(${i})">Update</button>
   
     </div>
     </div>
@@ -61,11 +62,11 @@ function displayTodos(){
        
             
             innerHtml+=`
-            <div class="flex flex-row justify-center mt-4 items-center ">
-            <input class="px-4 py-2 w-[220px] " value="${todos[i].todoText}" readonly>
-            <input class="px-3 py-2 w-[150px] mx-2"value=" ${todos[i].todoDate}">
-            <button class=" bg-pink-400 rounded-md py-2 px-3  font-semibold" onclick="deleteTodo(${i})">Delete</button> 
-            <button class=" bg-pink-400 rounded-md py-2 px-3 ml-2  font-semibold" onclick="editTodo(${i})">Edit</button> 
+            <div class="flex sm:flex-row sm:justify-center flex-col sm:mt-6 ">
+            <input class="px-4 py-2 sm:mx-0 mx-2 text-center " value="${todos[i].todoText}" readonly>
+            <input class="px-3 py-2 sm:mx-2 sm:m-0 m-2 text-center"value=" ${todos[i].todoDate}">
+            <button class=" bg-pink-400  py-2 px-3 my-1 sm:my-0 sm:mx-0 rounded-md mx-2  font-semibold" onclick="deleteTodo(${i})">Delete</button> 
+            <button class=" p-2 bg-pink-400 rounded-md  px-3 font-semibold sm:mx-0 sm:mb-0 sm:mx-2 mb-2 mx-2 sm:mt-0   mt-2" onclick="editTodo(${i})">Edit</button> 
             </div>
            
             `
