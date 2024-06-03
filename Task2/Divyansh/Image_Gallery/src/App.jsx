0ea266f8a1e6dc1ef363a70643ec6f8544d04eac
Divyansh Gupta <123203281+divyansh12git@users.noreply.github.com>
2024-06-03 12:03:16 +0530

    task 2 structure uploaded

---
+++ Task2/Divyansh/Image_Gallery/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex w-full  h-[100vh] justify-center items-center text-4xl text-teal-700 font-bold'> IMAGE GALLERY </div>
+       
+    </>
+  )
+}
+
+export default App
