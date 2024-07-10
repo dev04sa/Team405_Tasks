fd94c0fc3930ebf6140d0ec3e9f729f813502c69
RahulJangir84 <158582728+RahulJangir84@users.noreply.github.com>
2024-07-10 11:48:07 +0530

    changes done

--- Task3/Task3-1/Rahul/server.js
+++ Task3/Task3-1/Rahul/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-	response.write('Hello World and change the world\n');
+	response.write('Hello World\n');
 	response.end();
 });
 
