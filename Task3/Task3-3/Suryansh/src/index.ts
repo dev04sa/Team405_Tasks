7a84bed1728cf58f146ae5a67b7e1519a5a0c429
sahitya <sahityaaryansingh@gmail.com>
2024-07-18 15:27:14 +0530

    initial commit

--- Task3/Task3-3/Suryansh/src/index.ts
+++ Task3/Task3-3/Suryansh/src/index.ts
@@ -17,5 +17,5 @@ dbConnect();
 
 const PORT=process.env.PORT || 8080;
 app.listen(PORT,()=>{
-    console.log(`server is running on port ${PORT}`);
+    console.log(`server is listening  on port ${PORT}`);
 })
